fd2fbeb677335bf87334571930f5e3584edc8c8d
checoman7 <39683002+checoman7@users.noreply.github.com>
2018-06-02 23:45:07 -0500

    se borraron mensajes pelotudos

--- P/src/HolaMundo.java
+++ P/src/HolaMundo.java
@@ -3,21 +3,15 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola chocheman");
-		System.out.println("hola putito chocheman");
+	
 		newNoobs Patricia = new newNoobs("Patricia" , 10, true);
 		System.out.println(Patricia.getNobatin());
 		newNoobs Daniel = new newNoobs("Daniel" , 10, false);
 		System.out.println(Daniel.getNobatin());
-		System.out.println("Experimento CON CMD");
-		System.out.println("Sergio is from Arabia Saudita");
-		System.out.println("yo tambien estoy haciendo cambios!");
-		System.out.println("Sergio is from Arabia Sauditss");
-
+	
 		newNoobs Germin = new newNoobs ("Germin", 20, false);
 		System.out.println(Germin.getNobatin());
-		System.out.println("Patricia huele a popo");
-		System.out.println("de verdad ;)");
+	
 
 
 	}
